9cc86c3bd873e81bce491a22a633d4de0ea57461
Roy Storey <kiwiroy@users.noreply.github.com>
2018-03-08 21:44:25 +1300

    pod update

--- lib/Alien/KentSrc.pm
+++ lib/Alien/KentSrc.pm
@@ -20,9 +20,18 @@ __END__
 
 =head1 NAME
 
-Alien::KentSrc - Fetch/build/stash Kent src from http://hgdownload.cse.ucsc.edu/admin/
+Alien::KentSrc - Fetch/build/stash Kent src from L<http://hgdownload.cse.ucsc.edu/admin/>
 
 =begin HTML
+
+<img src="https://img.shields.io/badge/perl-5.8.9+-blue.svg"
+     alt="Requires Perl 5.8.9+" />
+
+<!-- CPAN -->
+<a href="https://metacpan.org/pod/Alien::KentSrc">
+  <img src="https://img.shields.io/cpan/v/Alien-KentSrc.svg"
+       alt="CPAN" />
+</a>
 
 <!-- Travis -->
 <a href="https://travis-ci.org/kiwiroy/alien-kentsrc">
@@ -56,5 +65,19 @@ defines the following new methods.
   Alien::KentSrc->machtype;
 
 Return the B<MACHTYPE> that I<jkweb.a> was built with.
+
+=head1 AUTHOR
+
+Roy Storey <kiwiroy@cpan.org>
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<http://hgdownload.cse.ucsc.edu/downloads.html#source_downloads>
+
+=item L<http://kentinformatics.com>
+
+=back
 
 =cut
